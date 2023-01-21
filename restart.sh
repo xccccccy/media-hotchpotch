dfc91b9a4a5c05c78827c61b141ff3c16bfe50f5
xiechenyao <xiechenyao.0820@bytedance.com>
2023-01-21 10:01:11 +0800

    .

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 if [ $(docker images | grep "none" | awk '{print $3}') ] ; then
     docker rmi $(docker images | grep "none" | awk '{print $3}')
 fi
-
+echo "compose start."
 docker compose up -d --build
