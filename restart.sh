2bf12e1bcffddb53c98358ebc0eb1233a438cb4b
xccccccy <xcy_0820@163.com>
2023-01-22 16:42:29 +0800

    1

--- restart.sh
+++ restart.sh
@@ -5,5 +5,5 @@ if [ $(docker images | grep "none" | awk '{print $3}' | head -1) ] ; then
     docker rmi $(docker images | grep "none" | awk '{print $3}')
 fi
 echo "docker compose start."
-export MYUID=$UID
+export MYUID=1001
 docker compose up -d --build
